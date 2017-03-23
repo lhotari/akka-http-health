ce1a264f55703348770f6eaab1a10c70ad4abd5a
Lari Hotari <lari@hotari.net>
2017-03-23 12:21:40 +0200

    Make Travis build publish signature files

--- travis/sign-released-files.sh
+++ travis/sign-released-files.sh
@@ -3,3 +3,5 @@ NAME=${1:-$(cat build.sbt | egrep '^name :=' | awk -F\" '{ print $2 }')}
 VERSION=${2:-$(cat build.sbt | egrep '^version :=' | awk -F\" '{ print $2 }')}
 echo "Signing released files..."
 curl -H "X-GPG-PASSPHRASE: $GPG_PASSPHRASE" "-u$BINTRAY_USER:$BINTRAY_API_KEY" -X POST "https://api.bintray.com/gpg/$BINTRAY_USER/releases/$NAME/versions/$VERSION"
+echo "Publishing signature files..."
+curl "-u$BINTRAY_USER:$BINTRAY_API_KEY" -X POST "https://api.bintray.com/content/$BINTRAY_USER/releases/$NAME/$VERSION/publish"
