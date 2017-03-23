6daa57b71aecd1a9f919589cb117940ce7cd3072
Lari Hotari <lari@hotari.net>
2017-03-23 16:41:54 +0200

    Add more logging to publishing

--- travis/sync-to-sonatype.sh
+++ travis/sync-to-sonatype.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 NAME=${1:-$(cat build.sbt | egrep '^name :=' | awk -F\" '{ print $2 }')}
 VERSION=${2:-$(cat build.sbt | egrep '^version :=' | awk -F\" '{ print $2 }')}
+echo "Syncing files to Sonatype OSS..."
 curl "-u$BINTRAY_USER:$BINTRAY_API_KEY" -H "Content-Type: application/json" \
 -d "{\"username\": \"$SONATYPE_USER\", \"password\": \"$SONATYPE_PASS\", \"close\": \"0\"}" \
 -X POST "https://api.bintray.com/maven_central_sync/$BINTRAY_USER/releases/$NAME/versions/$VERSION"
+echo -e "\nDone."
