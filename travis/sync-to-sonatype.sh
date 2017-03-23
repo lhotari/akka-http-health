411bd5ed8c1bff8d9258d0888fd0a1a15de37409
Lari Hotari <lari@hotari.net>
2017-03-23 12:38:31 +0200

    Make Travis build sync from Bintray to Sonatype OSS repo

---
+++ travis/sync-to-sonatype.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+NAME=${1:-$(cat build.sbt | egrep '^name :=' | awk -F\" '{ print $2 }')}
+VERSION=${2:-$(cat build.sbt | egrep '^version :=' | awk -F\" '{ print $2 }')}
+curl "-u$BINTRAY_USER:$BINTRAY_API_KEY" -H "Content-Type: application/json" \
+-d "{\"username\": \"$SONATYPE_USER\", \"password\": \"$SONATYPE_PASS\", \"close\": \"0\"}" \
+-X POST "https://api.bintray.com/maven_central_sync/$BINTRAY_USER/releases/$NAME/versions/$VERSION"
